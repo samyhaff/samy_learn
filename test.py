e4b1819caef775cc2936291a1cb93a132d217cf9
Samy Haffoudhi <haffoudhisamy@gmail.com>
2022-03-05 09:54:02 +0100

    test linear reg

---
+++ test.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from samy_learn import LinearRegression
+
+X = np.array([[0], [1], [2], [3]])
+y = np.array([0, 1.5, 1.9, 3])
+
+model = LinearRegression()
+model.fit(X, y)
+y_pred = model.predict(X)
+
+plt.plot(X, y, 'o', label='Data')
+plt.plot(X, y_pred, label='Linear Regression')
+plt.xlabel('x')
+plt.ylabel('y')
+plt.legend()
+plt.show()
