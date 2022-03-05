9672dbb9cc37d967866d6e18beb321576c7719dc
Samy Haffoudhi <haffoudhisamy@gmail.com>
2022-03-05 18:11:12 +0100

    cleaner test

---
+++ linear_reg_test.py
@@ -0,0 +1,13 @@
+import numpy as np
+from sklearn import datasets
+from samy_learn import LinearRegression, GDRegressor
+import matplotlib.pyplot as plt
+
+X, y = datasets.make_regression(n_samples=100, n_features=1, noise=20, random_state=42)
+model = GDRegressor()
+model.fit(X, y)
+y_pred = model.predict(X)
+
+plt.scatter(X, y)
+plt.plot(X, y_pred)
+plt.show()
